fc30477500eb0de9752078215cdc336c0fcd2e4e
vimaurya <mauryavikashhere@gmail.com>
2026-08-02 14:31:30 +0530

    standardise drift db url

--- internal/driver/factory.go
+++ internal/driver/factory.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/url"
 	"strings"
-
 )
 
 // func GetDriver(connURL string) (Driver, error) {
@@ -20,39 +19,71 @@ import (
 // 	return nil, fmt.Errorf("unsupported database scheme name in url : %s", connURL)
 // }
 
+type connectionUrl struct {
+	scheme string 
+	url string
+}
+
 func GetDriver(connURL string) (Driver, error) {
-	parts := strings.SplitN(connURL, "://", 2)	
-	if len(parts) > 2 {
-		return nil, fmt.Errorf("Some err")
-	}
-	
-	driver, err := getdbDriver(parts[0], connURL)
+	connectionURL, err := formatConnURL(connURL)
+
+	driver, err := getdbDriver(connectionURL.scheme, connectionURL.url)
 	if err!=nil{
 		return nil, err
 	}
 	
-	formatConnURL(connURL)
-
 	return driver, nil
 }
 
-func formatConnURL(connUrl string) (string, error){
+// Standard Drift URL format is : 
+// scheme://username:passowrd@host:port/database?queryparam=something
+
+// mysql://root:vikash@tcp(127.0.0.1:3306)/test_db
+// mysql://root:vikash@localhost:3306/test_db
+func formatConnURL(connUrl string) (connectionUrl, error){
 	parsedUrl, err := url.Parse(connUrl)	
 	if err!=nil{
-		return "", err
+		return connectionUrl{}, err
 	}
 	
-	fmt.Printf("inside formatting\n")
-	scheme := parsedUrl.Scheme
-	if scheme == "postgres"{
-		user := parsedUrl.User.Username()
-		password, _ := parsedUrl.User.Password()
-		host := parsedUrl.Host
-		database := parsedUrl.Path
-		query := parsedUrl.RawQuery
-		fmt.Printf("user : %s\npassword : %s\nhost : %s\n", user, password, host)
-		fmt.Printf("database : %s\nquery : %s\n", database, query)
-	}
+	conn := &connectionUrl{}
 
-	return "", nil
+	scheme := parsedUrl.Scheme
+
+	user := cleanString(parsedUrl.User.Username())
+	password, _ := parsedUrl.User.Password()
+	password = cleanString(password)
+	host := cleanString(parsedUrl.Host)
+	database := cleanString(parsedUrl.Path)
+	query := cleanString(parsedUrl.RawQuery)
+
+	// fmt.Printf("user : %s\npassword : %s\nhost : %s\n", user, password, host)
+	// fmt.Printf("database : %s\nquery : %s\n", database, query)
+	
+	var connectionString string
+	
+
+	if scheme == "postgres"{
+		connectionString = fmt.Sprintf("%s://%s:%s@%s/%s", scheme, user, password, host, database)
+		if query!=""{
+			connectionString += "?"+query
+		}
+	} else if scheme == "mysql" {
+			connectionString = fmt.Sprintf("%s://%s:%s@tcp(%s)/%s", scheme, user, password, host, database)	
+	}
+	
+	conn.scheme = scheme
+	conn.url = connectionString
+
+	return *conn, nil
+}
+
+func cleanString(dirtyString string) string{
+	clString,_ := strings.CutPrefix(dirtyString, ":")	
+	clString, _ = strings.CutSuffix(clString, ":")
+	clString, _ = strings.CutPrefix(clString, "/")
+	clString, _ = strings.CutSuffix(clString, "/")
+	clString, _ = strings.CutPrefix(clString, "@")
+	clString, _ = strings.CutSuffix(clString, "@")
+	return clString
 }
